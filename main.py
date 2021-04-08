1176f589934afddfdc1bb23b4208dc23c588d2a0
crja73 <57744842+crja73@users.noreply.github.com>
2021-04-08 12:52:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,51 +2,41 @@ from flask import Flask, render_template, redirect, request
 from data.loginform import LoginForm
 from data import db_session
 from data.users import User
-from data.registerform import RegisterForm
+from data.add_date import Add_date
 
-from data.safety import Security
 
 from data.user_function import user_func  # Отдельнвя функция инициализации базы данных
 from ctypes  import *
-sp = Security.set_password  # Функция превращения пароля в хеш
-ch = Security.check_password  # Функция проверки хеша и пароля
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-df = 'Москва, ул. Псковская, 11'
-city = df  # Адрес школы
-check1 = ''
-check2 = ''
-hash_password = ''
 email = ''
-flag_for_login = ''
-money = 0
-
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    global flag_for_login
-    global money
-    global hash_password
+
     global email
-    flag_for_login = False
+
     form = LoginForm()
     
     print(form.email.data)
     if form.email.data == 'test@gmail.com':  # Проверка почты
-           
-                
         return redirect('/safespace')
-
     else:
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
-
+@app.route('/safespace', methods=['GET', 'POST'])
+def SafeSpace():
+    form = Add_date()
+    ex =  request.form.get('text_box')
+    print(ex)
+    return render_template('safespace.html', title='koko') 
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
